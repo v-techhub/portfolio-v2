d13df6c949efe104df10ec39aea8817a205f91c3
Victor Adeshina <pianistpro8@gmail.com.com>
2026-06-07 23:04:22 +0100

    update lenis smooth scroll;

--- src/components/lenis-provider.tsx
+++ src/components/lenis-provider.tsx
@@ -4,7 +4,7 @@ import { ReactLenis } from "lenis/react";
 
 export default function LenisProvider({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true, syncTouch: true }}>
       {children}
     </ReactLenis>
   );
